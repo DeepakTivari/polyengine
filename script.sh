8304976ab130437253c6a2af2e7f8f7d64858f5d
Deepak Tivari <deepak.tivari@gmail.com>
2019-05-13 18:55:15 +0800

    working + housekeeping

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@
 #git clean -xdf
 make
 
- objdump -x virus | grep -E "main|decrypt"
+ objdump -x virus | grep -E "_start|main|decrypt"
 # echo polymake virus 5a0 30 563
 
 #./polymake virus 5d0 30 598
--- virus.asm
+++ virus.asm
@@ -57,22 +57,4 @@ _start:             ; Global entry point
 decrypt:
 	; there is automatic calculation happening inside DECRYPTOR_SECTION function which is (_start-main) == xxxh worth of memory that will be encrypted/decrypted
     DECRYPTOR_SECTION main, _start
-	; DECRYPTOR_SECTION main, 100h
-    ; DECRYPTOR_SECTION payload, payload.end-payload
     ret
-
-align 16
-payload:
-	mov eax,4            ; 'write' system call = 4
-	mov ebx,1            ; file descriptor 1 = STDOUT
-	mov ecx,hello        ; string to write
-	mov edx,helloLen     ; length of string to write
-	int 80h              ; call the kernel
-
-	; Terminate program
-	mov eax,1            ; 'exit' system call
-	mov ebx,0            ; exit with error code 0
-	int 80h              ; call the kernel
-
-align 16
-.end:
