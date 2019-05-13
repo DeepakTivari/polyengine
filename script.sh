17d9bf67cc4552fcf35fa16a63b07417b42dd747
zprimal <36151080+zprimal@users.noreply.github.com>
2019-05-13 20:55:23 +0800

    ayy

--- script.sh
+++ script.sh
@@ -8,6 +8,20 @@ make
 # objdump -x virus | grep -E "virus|decrypt"
 # echo polymake virus 5a0 30 563
 
+adrpayld=$(objdump -x virus | grep -E "payload" | head -n 1)
+
+
+adrpayldend=$(objdump -x virus | grep -E "payload.end")
+
+adrdecr=$(objdump -x virus | grep -E "decrypt.decryption_function")
+printf "payload start addr:\n"
+printf "%s " $adrpayld
+printf "\npayload end addr:\n"
+printf "%s " $adrpayldend
+printf "\npayload decryption addr:\n"
+printf "%s " $adrdecr
+echo
+
 ./polymake virus 5d0 30 598
 chmod +x virus_morph
 ./virus_morph
