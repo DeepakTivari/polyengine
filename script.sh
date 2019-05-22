94fe60df6ca3e018ed0ca0ac907328ae176def85
Deepak Tivari <deepak.tivari@gmail.com>
2019-05-22 15:15:53 +0800

    added structure change script

--- script.sh
+++ script.sh
@@ -1,5 +1,7 @@
 # objdump -x virus | grep -w -E "_start|main|decrypt_engine.decryption_function"
 chmod +x c_virus
 chmod +x asm_virus
-md5sum c_virus
 md5sum asm_virus
+md5sum c_virus
+
+objdump -S c_virus | grep $1
